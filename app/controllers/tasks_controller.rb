6032b1d9aca55b7c6dd0be0d4c30f2c1f30b219b
takezawamiki <mikiaaamiki@yahoo.co.jp>
2024-09-24 09:51:36 +0900

    Implementation of list screen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,32 @@
 class TasksController < ApplicationController
   def index
+      @tasks = Task.all
   end
 
   def new
       @task = Task.new
   end
-
+  
+  def create
+      @task = Task.new(task_params)
+      if @task.save
+          flash[:danger] = 'Taskを投稿しました'
+          redirect_to tasks_path
+      else
+          render :new, status: :unprocessable_entity
+      end
+  end
+  
   def show
   end
 
   def edit
+  end
+  
+  
+  private
+  
+  def task_params
+      params.require(:taks).permit(:title,:content)
   end
 end
